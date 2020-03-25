c007d2de5397efcee603d92273be496f514c1f79
Phan Trinh <pxt177@case.edu>
2020-03-24 22:33:11 -0400

    initial backend commit

---
+++ backend/weather.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ENDPOINT="api.openweathermap.org/data/2.5/weather?q="
+QUERY_STRING="city=cleveland&state=ohio"
+
+function setup_keys(){
+    source <(cat weather.key)
+}
+
+function query_map {
+    saveIFS=$IFS
+    IFS='=&'
+    query=($QUERY_STRING)
+    IFS=$saveIFS
+
+    declare -A query_map
+    for ((i=0; i<${#query[@]}; i+=2))
+    do
+        query_map[${query[i]}]=${query[i+1]}
+    done
+    echo ${query_map[@]}
+}
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+function weather_data {
+    param=$(query_map)
+    location=`join_by , $param`
+    query="${ENDPOINT}$location&appid=${API_KEY}"
+    echo $(curl -sS $query)
+}
+
+setup_keys
+weather_data
